751bd8ae4ca109cc2bf15bcd26413cb42d3e0dfd
lkzinfrost <lukascaipira192@gmail.com>
2025-11-16 00:48:25 -0300

    Add files via upload

---
+++ site-do-grupo-zap/script/script.js
@@ -0,0 +1,19 @@
+
+
+function toggleMenu() {
+    const menu = document.getElementById("menu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+
+function toggleDotsMenu() {
+    const menu = document.getElementById("dotsMenu");
+    menu.style.display = menu.style.display === "block" ? "none" : "block";
+}
+function toggleDotsMenu() {
+    let dotsMenu = document.getElementById("dotsMenu");
+    dotsMenu.style.display = dotsMenu.style.display === "block" ? "none" : "block";
+}
+function toggleDotsMenu() {
+    let dotsMenu = document.getElementById("dotsMenu");
+    dotsMenu.style.display = dotsMenu.style.display === "block" ? "none" : "block";
+}
